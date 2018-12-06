b212ddd4277bb64e9804c3a369779cc69a7e676f
narudesign_sekig <makoto.skgc@gmail.com>
2018-12-06 13:41:57 +0900

    Add OK and NG cases

--- Tests/SelectLayer.py
+++ Tests/SelectLayer.py
@@ -16,12 +16,35 @@ class selectLayer(lwsdk.ICommandSequence):
         super(selectLayer, self).__init__()
 
     def process(self, mod_command):
+
+        # OK
         cs_options = lwsdk.marshall_dynavalues('1 2 4')
+
+        # OK
+        # cs_options = lwsdk.marshall_dynavalues(' '.join(["1", "2", "4"]))
+
+        # NG case
+        # cs_options = lwsdk.marshall_dynavalues(["1", "2", "4"])
+
+        # NG case
+        # layerList = []
+        # layerList.append(str(1))
+        # layerList.append(str(2))
+        # layerList.append(str(4))
+        # cs_options = lwsdk.marshall_dynavalues(layerList)
+
+        # NG case
+        # layerList = []
+        # layerList.append(1)
+        # layerList.append(2)
+        # layerList.append(4)
+        # cs_options = lwsdk.marshall_dynavalues(layerList)
+
         cs_setlayer = mod_command.lookup(mod_command.data, "SETALAYER")
         result, dyna_value = mod_command.execute(
             mod_command.data, cs_setlayer, cs_options, lwsdk.OPSEL_USER)
 
-        cs_options = lwsdk.marshall_dynavalues('10')
+        cs_options = lwsdk.marshall_dynavalues('3 10')
         cs_setlayer = mod_command.lookup(mod_command.data, "setblayer")
         result, dyna_value = mod_command.execute(
             mod_command.data, cs_setlayer, cs_options, lwsdk.OPSEL_USER)
@@ -33,6 +56,8 @@ ServerTagInfo = [
     ("Select Layer By Name", lwsdk.SRVTAG_USERNAME | lwsdk.LANGID_USENGLISH),
     ("Select Layer", lwsdk.SRVTAG_BUTTONNAME | lwsdk.LANGID_USENGLISH),
     ("Utilities/Python", lwsdk.SRVTAG_MENU | lwsdk.LANGID_USENGLISH)
+
+
 ]
 
 ServerRecord = {lwsdk.CommandSequenceFactory(
